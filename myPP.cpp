44ab6b2994f8a1bd79f8498b0db7328dfb69bb77
Sankarshan Araujo <sa343@uakron.edu>
2021-03-14 11:07:01 -0400

    Remove STL dependencies

--- myPP.cpp
+++ myPP.cpp
@@ -1,34 +1,55 @@
 /*
-    My data Pointer implementation file
+    My parent Pointer implementation file
 */
 
 #include "myPP.hpp"
 #include <iostream>
 
-// use a vector
-// ex: vector<int> set(100); set of 100 nodes all 0's 
-// ex: vector<int> set = {1, 2, 3, 4}; make a set of 4 nodes numbered as node 1, node 2,... etc
+// create array of size 'size'
 // set depth of each node to 0 (default depth of a node/single-element set)
-void myPP::create(std::vector<int> const& set)
+
+myPP::myPP(int size)
 {
-    for (int i: set)
+    parent = new int[size];
+    depth = new int[size];
+
+    for (int i = 0; i < size; i++)
     {
-        data[i] = i;
+        parent[i] = i;
         depth[i] = 0;
     }
 }
+
+// destructor
+myPP::~myPP()
+{
+    if (parent != nullptr && depth != nullptr)
+    {
+        delete [] parent;
+        parent = nullptr;
+        delete [] depth;
+        depth = nullptr;
+    }
+}
+
+void myPP::create(int size)
+{
+    parent = new int[size];
+    depth = new int[size];
+}
+
 // give an element and finds the root
 int myPP::Find(int element)
 {
     // if element isn't the root
-    if (data[element] != element)
+    if (parent[element] != element)
     {
         // path compression
         // makes nodes that have the same root node but not the same parent have the same parent (the root)
-        data[element] = Find(data[element]);
+        parent[element] = Find(parent[element]);
     }
 
-    return data[element];
+    return parent[element];
 }
 
 void myPP::Union(int element1, int element2)
@@ -40,22 +61,22 @@ void myPP::Union(int element1, int element2)
     // if they are from the same root, return
     if (x == y)
     {
-        std::clog << "These elements have the same root\n";
+        // std::clog << "These elements have the same root\n";
         return;
     }
     // weighted union
     // attach smaller depth tree to the tree with larger depth
     if (depth[x] > depth[y])
     {
-        data[y] = x;
+        parent[y] = x;
     }
     else if (depth[x] < depth[y])
     {
-        data[x] = y;
+        parent[x] = y;
     }
     else
     {
-        data[x] = y;
+        parent[x] = y;
         depth[y]++;
     }
 }
--- myPP.hpp
+++ myPP.hpp
@@ -2,23 +2,20 @@
     My Parent Pointer declaration file
 */
 
-#include <unordered_map>
-#include <vector>
-
-class myPP 
+class myPP
 {
 
 private:
 
-    std::unordered_map<int, int> data; // nodes and information in the tree as a hash map
-    std::unordered_map<int, int> depth; // rank/depth of the tree
+    int *parent;
+    int *depth;
 
 public:
 
-    myPP() {} // def constructor
-    ~myPP() {} // def destructor
+    myPP(int); // def constructor
+    ~myPP(); // destructor
     int Find(int element); // find the parent of the element
     void Union(int element1, int element2); // merge two disjoint sets together given two elements
-    void create(std::vector<int> const& set); // create a parent pointer tree given a set/vector
+    void create(int size); // create a parent pointer tree given a set/vector
 
 };
